7731cba10b3d1c9c3c0e3d93c421e60a5139ef3e
pradeep <pradeep.ncst@gmail.com>
2020-07-02 22:51:42 -0400

    first sprimng security

---
+++ 01-spring-security-basic/src/main/java/com/spring/security/HelloController.java
@@ -0,0 +1,16 @@
+package com.spring.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String sayHello(String name) {
+		return "Hello "+name;
+	}
+	
+	
+
+}
