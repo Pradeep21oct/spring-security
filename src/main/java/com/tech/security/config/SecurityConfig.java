833509d64557ba117ae6b6e1c7940f88cbda727e
pradeep <pradeep.ncst@gmail.com>
2025-03-02 10:09:59 -0500

    added CSRF token

--- src/main/java/com/tech/security/config/SecurityConfig.java
+++ src/main/java/com/tech/security/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.tech.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,7 +14,12 @@ public class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity httpsec) throws Exception {
-        return httpsec.build();
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http.csrf(customizer->customizer.disable())
+                .authorizeHttpRequests(request->request.anyRequest().authenticated())
+              //  .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 }
