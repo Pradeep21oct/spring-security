79f5a42120168e33a610989966d5f1765b6f0e4c
Shreyashi Singh <shreyashi01.singh@gmail.com>
2025-03-01 22:59:39 -0500

    Spring HelloController

--- src/main/java/com/tech/security/HelloController.java
+++ src/main/java/com/tech/security/HelloController.java
@@ -1,15 +1,25 @@
 package com.tech.security;
 
+import com.tech.security.model.Student;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class HelloController {
+    List<Student> student=new ArrayList<>(List.of(new Student(1,"Pradeep",20),new Student(2,"Ramesh",30)));
     @GetMapping("hello")
     public String hello(HttpServletRequest http){
         return "Hello "+http.getRequestedSessionId();
 
     }
+@PostMapping("addStudent")
+public List<Student> addStudent(@RequestBody Student std){
+       student.add(std);
+       return student;
+
+}
 }
---
+++ src/main/java/com/tech/security/model/Student.java
@@ -0,0 +1,21 @@
+package com.tech.security.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    public int id;
+    public  String name;
+    public int age;
+
+
+    public Student(int id, String name, int age) {
+        this.id=id;
+        this.name=name;
+       this.age=age;
+    }
+}
